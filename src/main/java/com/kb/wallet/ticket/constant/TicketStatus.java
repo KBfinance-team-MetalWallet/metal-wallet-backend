e42e61835bf44ece7e9dbed3b0dd2c98c93aa1d6
seowonn <sonseowon5@gmail.com>
2024-09-16 16:35:57 +0900

    Feat: 티켓 엔티티 생성 (멤버 매핑 관계만 추가) - 나머지 매핑 추가 필요

---
+++ src/main/java/com/kb/wallet/ticket/constant/TicketStatus.java
@@ -0,0 +1,17 @@
+package com.kb.wallet.ticket.constant;
+
+import lombok.Getter;
+
+@Getter
+public enum TicketStatus {
+  BOOKED("BOOKED"),
+  CANCELED("CANCELED"),
+  EXCHANGE_REQUESTED("EXCHANGE_REQUESTED"),
+  CHECKED("CHECKED");
+
+  private final String status;
+
+  TicketStatus(String status) {
+    this.status = status;
+  }
+}
---
+++ src/main/java/com/kb/wallet/ticket/domain/Ticket.java
@@ -0,0 +1,50 @@
+package com.kb.wallet.ticket.domain;
+
+import com.kb.wallet.member.domain.Member;
+import com.kb.wallet.ticket.constant.TicketStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+@Entity
+@Table(name = "ticket")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ticket {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @Enumerated(value = EnumType.STRING)
+  // enum의 값을 index가 아닌 텍스트 값 그대로 저장하고 싶을 때 위의 어노테이션 사용
+  private TicketStatus ticketStatus;
+
+  @CreatedDate
+  private LocalDateTime createdAt;
+
+  private LocalDateTime validUntil;
+
+  @Column
+  private LocalDateTime cancelUntil;
+
+}
