32b7eeaacabc60542591b3a8868072d318ffdd08
veniharuka <kurobas1384@gmail.com>
2024-09-19 10:05:16 +0900

    Revert "Merge pull request #2 from KBfinance-team-MetalWallet/fix/member/entity/lhh"
    This reverts commit d1e652b6285d77414fe5a95f89af09bf623211d6, reversing
    changes made to 8e7276fc8f7f3577677290dfe66680e86dec8b9d.

--- src/main/java/com/kb/wallet/member/domain/Member.java
+++ src/main/java/com/kb/wallet/member/domain/Member.java
@@ -33,7 +33,7 @@ public class Member {
 
   @Column(unique = true)
   @NotNull(message = "전화번호는 필수 입력 항목입니다.")
-  @Size(min = 11, max = 11, message = "전화번호는 숫자(11자리)만 입력해주세요.")
+  @Size(min = 11, max = 11, message = "전화번호는 000-0000-0000 형식이어야 합니다.")
   private String phone;
 
   @NotNull(message = "핀번호는 필수 입력 항목입니다.")
