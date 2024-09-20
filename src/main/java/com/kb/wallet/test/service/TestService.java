8865e85b638c56692d4181718b0043cb56653b3b
leehyeonhee <2hyeonheedev@gmail.com>
2024-09-20 22:41:42 +0900

    Feat: 멤버 테스트 API 이전
    - GET /api/test/members/{id} 이전
    - POST /api/test/members 이전
    - Spring Security Filter를 /api/test/**는 통과할 수 있도록 관리하기 위함

---
+++ src/main/java/com/kb/wallet/test/service/TestService.java
@@ -0,0 +1,14 @@
+package com.kb.wallet.test.service;
+
+import com.kb.wallet.member.domain.Member;
+import java.util.Optional;
+
+public interface TestService {
+
+    Optional<Member> getMemberById(Long id);
+
+    Optional<Member> getMemberById2(Long id);
+
+    Member createMember(Member member);
+
+}
---
+++ src/main/java/com/kb/wallet/test/service/TestServiceImpl.java
@@ -0,0 +1,32 @@
+package com.kb.wallet.test.service;
+
+import com.kb.wallet.member.domain.Member;
+import com.kb.wallet.member.repository.MemberMapper;
+import com.kb.wallet.member.repository.MemberRepository;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TestServiceImpl implements TestService {
+
+    private final MemberRepository memberRepository;
+    private final MemberMapper memberMapper;
+
+    @Override
+    public Optional<Member> getMemberById(Long id) {
+        return memberRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Member> getMemberById2(Long id) {
+        Member member = memberMapper.findById(id);
+        return Optional.ofNullable(member);
+    }
+
+    @Override
+    public Member createMember(Member member) {
+        return memberRepository.save(member);
+    }
+}
