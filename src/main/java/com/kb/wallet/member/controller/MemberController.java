ec07b69dda938f8fddf6e9600e22711cfe1c39c4
leehyeonhee <2hyeonheedev@gmail.com>
2024-09-22 14:02:37 +0900

    Feat: 회원가입 및 로그인 API 구현

--- src/main/java/com/kb/wallet/member/controller/MemberController.java
+++ src/main/java/com/kb/wallet/member/controller/MemberController.java
@@ -1,49 +1,78 @@
 package com.kb.wallet.member.controller;
 
+import com.kb.wallet.jwt.JwtFilter;
+import com.kb.wallet.jwt.TokenProvider;
 import com.kb.wallet.member.domain.Member;
-import com.kb.wallet.member.service.MemberServiceImpl;
+import com.kb.wallet.member.dto.request.LoginMemberRequest;
+import com.kb.wallet.member.dto.request.RegisterMemberRequest;
+import com.kb.wallet.member.dto.response.RegisterMemberResponse;
+import com.kb.wallet.member.service.MemberService;
+import java.util.HashMap;
+import javax.validation.Valid;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
 @RestController
 @RequestMapping("/members")
 @Slf4j
+@AllArgsConstructor
 public class MemberController {
 
-    private final MemberServiceImpl memberService;
+    private final MemberService memberService;
 
-    @Autowired
-    public MemberController(MemberServiceImpl memberService) {
-        this.memberService = memberService;
+    @Lazy
+    private final TokenProvider tokenProvider;
+    private final PasswordEncoder encoder;
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterMemberResponse> registerMember(
+            @RequestBody @Valid RegisterMemberRequest request) {
+        log.info("Registering member: {}", request);
+        RegisterMemberResponse response = memberService.registerMember(request);
+        return ResponseEntity.ok(response);
     }
 
-    /**
-     * repository use
-     * Optional<Member> member = memberService.getMemberById(id);
-     *
-     * mapper use
-     * Optional<Member> member = memberService.getMemberById2(id);
-     *
-     * @param id
-     * @return
-     */
-    @GetMapping("/{id}")
-    public ResponseEntity<Member> getMemberById(@PathVariable("id") Long id) {
-        log.info("Finding member by id: {}", id);
+    @PostMapping("/login")
+    public ResponseEntity<HashMap<String, Object>> loginMember(
+            @RequestBody @Valid LoginMemberRequest request) {
+        log.info("Login member: {}", request);
 
-        Optional<Member> member = memberService.getMemberById(id);
+        HashMap<String, Object> map = new HashMap<>();
+        Member member = memberService.getMemberByEmail(request.getEmail());
 
-        return member.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
-    }
+        // 사용자 체크
+        if (member == null) {
+            map.put("result", "fail");
+            map.put("email", request.getEmail());
+            map.put("message", "사용자를 찾을 수 없습니다.");
+            return new ResponseEntity<>(map, HttpStatus.OK);
+        }
 
-    @PostMapping
-    public ResponseEntity<Member> createMember(@RequestBody Member member) {
-        log.info("Inserting new member: {}", member);
-        Member savedMember = memberService.createMember(member);
-        return ResponseEntity.ok(savedMember);
+        // 패스워드 체크
+        if (!encoder.matches(request.getPassword(), member.getPassword())) {
+            map.put("result", "fail");
+            map.put("email", request.getEmail());
+            map.put("message", "패스워드가 일치하지 않습니다");
+            return new ResponseEntity<>(map, HttpStatus.OK);
+        }
+
+        String accessToken = tokenProvider.createToken(member.getEmail(), member.getRole().name());
+
+        map.put("result", "success");
+        map.put("accessToken", accessToken);
+
+        // 헤더에 정보 추가
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + accessToken);
+        return new ResponseEntity<>(map, httpHeaders, HttpStatus.OK);
     }
 }
--- src/main/java/com/kb/wallet/member/repository/MemberRepository.java
+++ src/main/java/com/kb/wallet/member/repository/MemberRepository.java
@@ -1,7 +1,14 @@
 package com.kb.wallet.member.repository;
 
 import com.kb.wallet.member.domain.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByPhone(String phone);
+
+    Optional<Member> getByEmail(String email);
 }
