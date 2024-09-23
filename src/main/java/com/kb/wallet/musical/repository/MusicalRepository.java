a40d40b3bcebe3e335ec013810d3484f65d708dc
chuseok <90127lucy@gmail.com>
2024-09-23 16:50:16 +0900

    Fix: Musical 테이블 자동 생성 오류 수정 Rank -> Ranking 컬럼명 수정 후 Merge

--- src/main/java/com/kb/wallet/musical/repository/MusicalRepository.java
+++ src/main/java/com/kb/wallet/musical/repository/MusicalRepository.java
@@ -1,26 +1,19 @@
 package com.kb.wallet.musical.repository;
 
 import com.kb.wallet.musical.domain.Musical;
-import jakarta.transaction.Transactional;
 import java.util.Optional;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MusicalRepository extends JpaRepository<Musical, Long> {
+
     Page<Musical> findAll(Pageable pageable);
 
     @Override
     Optional<Musical> findById(Long musicalId);
-
-
-
-
 
 }
 
