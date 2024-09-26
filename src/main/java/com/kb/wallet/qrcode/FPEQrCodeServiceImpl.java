52b8ff3275942dd48ca7bf4bfedbd922fa73528d
veniharuka <kurobas1384@gmail.com>
2024-09-26 23:23:38 +0900

    Style:FPEQrCodeServiceImpl에 대한 오류 처리 추가
    - 암호화 및 복호화 과정 중 CustomException 및 ErrorCode를 사용하여 예외 처리 추가

--- src/main/java/com/kb/wallet/qrcode/FPEQrCodeServiceImpl.java
+++ src/main/java/com/kb/wallet/qrcode/FPEQrCodeServiceImpl.java
@@ -1,5 +1,10 @@
 package com.kb.wallet.qrcode;
 
+import static com.kb.wallet.global.common.status.ErrorCode.DECRYPTION_ERROR;
+import static com.kb.wallet.global.common.status.ErrorCode.ENCRYPTION_ERROR;
+import static com.kb.wallet.global.common.status.ErrorCode.INVALID_TYPE_VALUE;
+
+import com.kb.wallet.global.exception.CustomException;
 import com.kb.wallet.global.util.BouncyCastleUtil;
 import java.util.Base64;
 import javax.crypto.SecretKey;
@@ -7,6 +12,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class FPEQrCodeServiceImpl implements FPEQrCodeService {
+
+  public static final int MAX_IV_INT = 16;
 
   private final SecretKey secretKey;
   private final byte[] iv;
@@ -17,21 +24,27 @@ public class FPEQrCodeServiceImpl implements FPEQrCodeService {
   }
 
   @Override
-  public EncrypeDataDto encrypt(String data) throws Exception {
-    String encryptedData = BouncyCastleUtil.encrypt(data, secretKey, iv);
-    String secretKeyString = Base64.getEncoder().encodeToString(secretKey.getEncoded());
+  public EncrypeDataDto encrypt(String data) {
+    validateInputData(data);
+    try {
+      String encryptedData = BouncyCastleUtil.encrypt(data, secretKey, iv);
+      String secretKeyString = Base64.getEncoder().encodeToString(secretKey.getEncoded());
 
-    EncrypeDataDto dto = new EncrypeDataDto();
-    dto.setEncryptedData(encryptedData);
-    dto.setSecurityCode(secretKeyString);
-    dto.setIv(iv);
+      return new EncrypeDataDto(encryptedData, secretKeyString, iv);
 
-    return dto;
+    } catch (Exception e) {
+      throw new CustomException(ENCRYPTION_ERROR, "암호화 중 오류가 발생했습니다.");
+    }
   }
 
   @Override
   public String decrypt(EncrypeDataDto dto) throws Exception {
-    return BouncyCastleUtil.decrypt(dto.getEncryptedData(), secretKey, dto.getIv());
+    validateEncryptedData(dto.getEncryptedData());
+    try {
+      return BouncyCastleUtil.decrypt(dto.getEncryptedData(), secretKey, dto.getIv());
+    } catch (Exception e) {
+      throw new CustomException(DECRYPTION_ERROR, "복호화 중 오류가 발생했습니다.");
+    }
   }
 
   @Override
@@ -39,32 +52,31 @@ public class FPEQrCodeServiceImpl implements FPEQrCodeService {
     return Base64.getEncoder().encodeToString(secretKey.getEncoded());
   }
 
-  // 유효성 검사 메서드 구현
   @Override
   public void validateSecretKey(String secretKey) {
     if (secretKey == null || secretKey.isEmpty()) {
-      throw new IllegalArgumentException("Secret key는 null이거나 비어 있을 수 없습니다.");
+      throw new CustomException(INVALID_TYPE_VALUE, "유효하지 않은 Secret Key입니다.");
     }
   }
 
   @Override
   public void validateIv(byte[] iv) {
-    if (iv == null || iv.length != 16) {
-      throw new IllegalArgumentException("IV는 null이거나 16바이트여야 합니다.");
+    if (iv == null || iv.length != MAX_IV_INT) {
+      throw new CustomException(INVALID_TYPE_VALUE, "유효하지 않은 IV입니다. IV는 16바이트여야 합니다.");
     }
   }
 
   // 입력 데이터 유효성 검사
   private void validateInputData(String data) {
     if (data == null || data.isEmpty()) {
-      throw new IllegalArgumentException("암호화할 데이터는 null이거나 비어 있을 수 없습니다.");
+      throw new CustomException(INVALID_TYPE_VALUE, "암호화할 데이터는 null이거나 비어 있을 수 없습니다.");
     }
   }
 
   // 암호화된 데이터 유효성 검사
   private void validateEncryptedData(String encryptedData) {
     if (encryptedData == null || encryptedData.isEmpty()) {
-      throw new IllegalArgumentException("복호화할 데이터는 null이거나 비어 있을 수 없습니다.");
+      throw new CustomException(INVALID_TYPE_VALUE, "복호화할 데이터는 null이거나 비어 있을 수 없습니다.");
     }
   }
 }
