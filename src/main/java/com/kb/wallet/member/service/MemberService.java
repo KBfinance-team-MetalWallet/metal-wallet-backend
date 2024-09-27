88883f7c503c2c0f2a1787e7892eb9666c817531
chuseok <90127lucy@gmail.com>
2024-09-27 15:37:02 +0900

    Feat: member findById 로직 추가

--- src/main/java/com/kb/wallet/member/service/MemberService.java
+++ src/main/java/com/kb/wallet/member/service/MemberService.java
@@ -2,11 +2,11 @@ package com.kb.wallet.member.service;
 
 import com.kb.wallet.member.domain.Member;
 import com.kb.wallet.member.dto.request.RegisterMemberRequest;
+import com.kb.wallet.member.dto.response.MemberResponse;
 import com.kb.wallet.member.dto.response.RegisterMemberResponse;
 
 public interface MemberService {
-
+    MemberResponse findById(Long memberId);
     RegisterMemberResponse registerMember(RegisterMemberRequest request);
-
     Member getMemberByEmail(String email);
 }
