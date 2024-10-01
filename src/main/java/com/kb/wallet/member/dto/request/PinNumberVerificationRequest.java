0ad87f06a80affb8b2f69f882aaa9cd8f3d5545b
seowonn <sonseowon5@gmail.com>
2024-10-01 17:06:42 +0900

    Feat: 핀번호 확인 api 추가

---
+++ src/main/java/com/kb/wallet/member/dto/request/PinNumberVerificationRequest.java
@@ -0,0 +1,13 @@
+package com.kb.wallet.member.dto.request;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class PinNumberVerificationRequest {
+  @NotBlank(message = "핀번호는 필수 입력 항목입니다.")
+  @Size(min = 6, max = 6, message = "핀번호는 6자리여야 합니다.")
+  private String pinNumber;
+
+}
