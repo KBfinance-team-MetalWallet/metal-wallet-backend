bd8ebdf07eb3e3991e669f730250e206f8fb8099
veniharuka <kurobas1384@gmail.com>
2024-10-04 10:04:12 +0900

    Feat(DTO): 서명 및 검증을 위한 Request/Response DTO 추가
    - SignedTicketRequest 생성
    - SignedTicketResponse 생성
    - VerifyTicketRequest 생성
    - VerifyTicketResponse 생성

---
+++ src/main/java/com/kb/wallet/ticket/dto/request/SignedTicketRequest.java
@@ -0,0 +1,15 @@
+package com.kb.wallet.ticket.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class SignedTicketRequest {
+
+  private Long ticketId;
+}
