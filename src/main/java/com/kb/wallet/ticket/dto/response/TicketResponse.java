066ec6ebce74980585396a2a06899d179740b854
veniharuka <kurobas1384@gmail.com>
2024-10-04 10:01:34 +0900

    Feat(Ticket): deviceId 필드 추가
    기기 식별을 위한
    - TicketRequest에 deviceId 필드 추가
    - TicketResponse에 deviceId 필드 추가

--- src/main/java/com/kb/wallet/ticket/dto/response/TicketResponse.java
+++ src/main/java/com/kb/wallet/ticket/dto/response/TicketResponse.java
@@ -20,6 +20,7 @@ public class TicketResponse {
   private String createdAt;
   private String validUntil;
   private String cancelUntil;
+  private String deviceId;
 
   public static TicketResponse toTicketResponse(Ticket ticket) {
     return TicketResponse.builder()
@@ -28,6 +29,7 @@ public class TicketResponse {
       .createdAt(ticket.getCreatedAt().toString())
       .validUntil(ticket.getValidUntil().toString())
       .cancelUntil(ticket.getCancelUntil().toString())
+      .deviceId(ticket.getDeviceId())
       .build();
   }
 
