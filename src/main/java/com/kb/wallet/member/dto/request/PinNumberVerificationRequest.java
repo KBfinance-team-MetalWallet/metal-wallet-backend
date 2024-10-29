ca158bd6d56668a4980cd39d6a104b61f57a3f8b
leehyeonhee <2hyeonheedev@gmail.com>
2024-10-29 23:55:07 +0900

    Refactor: 생성자 메서드 대신 @AllArgsConstructor 추가

--- src/main/java/com/kb/wallet/member/dto/request/PinNumberVerificationRequest.java
+++ src/main/java/com/kb/wallet/member/dto/request/PinNumberVerificationRequest.java
@@ -2,16 +2,14 @@ package com.kb.wallet.member.dto.request;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public class PinNumberVerificationRequest {
 
   @NotBlank(message = "핀번호는 필수 입력 항목입니다.")
   @Size(min = 6, max = 6, message = "핀번호는 6자리여야 합니다.")
   private String pinNumber;
-
-  public PinNumberVerificationRequest(String pinNumber) {
-    this.pinNumber = pinNumber;
-  }
 }
