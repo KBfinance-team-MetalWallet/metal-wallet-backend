feb3f86f47e0ceafca3546fd9031914818397fff
seowonn <sonseowon5@gmail.com>
2024-11-01 11:01:55 +0900

    Chore: 안쓰는 import문 제거

--- src/test/java/com/kb/wallet/ticket/service/ScheduleServiceImplTest.java
+++ src/test/java/com/kb/wallet/ticket/service/ScheduleServiceImplTest.java
@@ -2,7 +2,6 @@ package com.kb.wallet.ticket.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.longThat;
 import static org.mockito.BDDMockito.given;
 
 import com.kb.wallet.ticket.domain.Schedule;
