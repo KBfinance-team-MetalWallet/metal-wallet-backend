efe3338c9e0bb1eb4c3c5934f2d57a4eb3cbcc1c
seowonn <sonseowon5@gmail.com>
2024-11-01 09:31:47 +0900

    Test: Section 도메인 테스트 작성

--- src/main/java/com/kb/wallet/seat/domain/Section.java
+++ src/main/java/com/kb/wallet/seat/domain/Section.java
@@ -15,14 +15,18 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
+@Builder
 public class Section {
 
   @Id
