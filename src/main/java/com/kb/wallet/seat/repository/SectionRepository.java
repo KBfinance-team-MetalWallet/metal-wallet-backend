17551e2c06693423b6e4953f2a2cf8c0bbbb92c2
seowonn <sonseowon5@gmail.com>
2024-11-04 22:16:17 +0900

    Test: SeatRepository 테스트 코드 작성 - SeatRepository 생성

---
+++ src/main/java/com/kb/wallet/seat/repository/SectionRepository.java
@@ -0,0 +1,8 @@
+package com.kb.wallet.seat.repository;
+
+import com.kb.wallet.seat.domain.Section;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SectionRepository extends JpaRepository<Section, Long> {
+
+}
