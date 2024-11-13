e5df38bead4611adb102eca7a206b3bf063291a4
leehyeonhee <2hyeonheedev@gmail.com>
2024-11-13 23:00:28 +0900

    Refactor: 파일명 변경

--- src/test/java/com/kb/wallet/ticket/service/TicketServiceConcurrencyTest.java
+++ src/test/java/com/kb/wallet/ticket/service/TicketServiceConcurrencyTest.java
@@ -1,248 +1,208 @@
 package com.kb.wallet.ticket.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.kb.wallet.global.config.AppConfig;
-import com.kb.wallet.member.constant.RoleType;
-import com.kb.wallet.member.domain.Member;
-import com.kb.wallet.member.repository.MemberRepository;
-import com.kb.wallet.musical.domain.Musical;
-import com.kb.wallet.musical.repository.MusicalRepository;
-import com.kb.wallet.seat.constant.Grade;
-import com.kb.wallet.seat.domain.Seat;
-import com.kb.wallet.seat.domain.Section;
-import com.kb.wallet.seat.repository.SeatRepository;
-import com.kb.wallet.ticket.domain.Schedule;
 import com.kb.wallet.ticket.dto.request.TicketRequest;
 import com.kb.wallet.ticket.dto.response.TicketResponse;
-import com.kb.wallet.ticket.repository.ScheduleRepository;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
-import javax.persistence.EntityManager;
-import javax.transaction.Transactional;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-//테스트 인스턴스를 클래스 수준에서 관리하도록 하고, 각 테스트 메소드 간에 데이터가 유지되도록
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {
   AppConfig.class
 })
 @WebAppConfiguration
-@Transactional
-public class TicketServiceConcurrencyTest {
-
-  @Autowired
-  private MemberRepository memberRepository;
-
-  @Autowired
-  private SeatRepository seatRepository;
+class TicketServiceConcurrencyTest {
 
   @Autowired
   private TicketService ticketService;
 
   @Autowired
-  private MusicalRepository musicalRepository;
+  private JdbcTemplate jdbcTemplate;
 
-  @Autowired
-  private ScheduleRepository scheduleRepository;
+  @BeforeEach
+  public void setUp() {
+    // member 데이터 삽입
+    jdbcTemplate.execute(
+      "insert into member (id, email, is_activated, name, password, phone, pin_number, role) values "
+        +
+        "(1, 'test1@gmail.com', true, 'test1', 'password1', '01011111111', '111111', 'USER')");
 
-  @Autowired
-  private EntityManager em;
-  private static final int availableSeats = 2;
+    jdbcTemplate.execute(
+      "insert into member (id, email, is_activated, name, password, phone, pin_number, role) values "
+        +
+        "(2, 'test2@gmail.com', true, 'test2', 'password2', '01022222222', '222222', 'USER')");
 
-  private final String deviceId = "device123";
+    // musical 데이터 삽입
+    jdbcTemplate.execute(
+      "insert into musical (id, place, place_detail, ranking, running_time, ticketing_end_date, ticketing_start_date, title, detail_image_url, notice_image_url, place_image_url, poster_image_url) values "
+        +
+        "(1, '서울', '서울 아트센터', 1, 150, '2024-10-16', '2024-10-01', '킹키부츠', null, null, null, null)");
 
-  private static final String[] EMAILS = new String[11];
+    // schedule 데이터 삽입
+    jdbcTemplate.execute(
+      "insert into schedule (id, date, start_time, end_time, musical_id) values " +
+        "(1, '2024-10-17', '10:00:00', '12:30:00', 1)");
 
-  static {
-    for (int i = 0; i < EMAILS.length; i++) {
-      EMAILS[i] = "user" + (i + 1) + "@example.com";
-    }
+    // section 데이터 삽입
+    jdbcTemplate.execute(
+      "insert into section (id, available_seats, grade, price, musical_id, schedule_id) values " +
+        "(1, 100, 'R', 190000, 1, 1)");
+
+    // seat 데이터 삽입
+    jdbcTemplate.execute(
+      "insert into seat (id, is_available, seat_no, schedule_id, section_id) values " +
+        "(1, true, 1, 1, 1)");
   }
 
-  static List<Member> members;
-  static List<Seat> seats;
-  private Musical testMusical;
-  private Section testSection;
-  private Schedule testSchedule;
-  private boolean isInitialized = false;
+  @AfterEach
+  void tearDown() {
+    cleanUpAll();
+  }
 
-  @Test
-  @DisplayName("단건 티켓을 예매할 경우, 예몌가 성공한다.")
-  public void testBookTicket_singleTicketSuccress() {
-    Long seatId = 2L;
-    TicketRequest ticketRequest = new TicketRequest();
-    ticketRequest.setSeatId(List.of(seatId));
-    ticketRequest.setDeviceId(deviceId);
-
-    Member member = Member.builder()
-      .email(EMAILS[0])
-      .name("User" + (0 + 1))
-      .phone("010123456" + 0)
-      .password("password")
-      .pinNumber("1234")
-      .role(RoleType.USER)
-      .isActivated(true)
-      .build();
-    memberRepository.save(member);
-
-    testMusical = createTestMusical(musicalRepository);
-    testSchedule = createTestSchedule(scheduleRepository, testMusical);
-    testSection = Section.builder()
-      .grade(Grade.R)
-      .availableSeats(availableSeats)
-      .build();
-    em.persist(testSection);
-    em.flush();
-
-    Seat seat = Seat.builder()
-      .isAvailable(true)
-      .schedule(testSchedule)
-      .section(testSection)
-      .build();
-    seatRepository.save(seat);
-
-    List<TicketResponse> ticketResponses = ticketService.bookTicket(EMAILS[0], ticketRequest);
-
-    assertThat(ticketResponses)
-      .isNotNull()
-      .isNotEmpty()
-      .withFailMessage("티켓 응답이 비어 있지 않아야 합니다.")
-      .hasSize(1)
-      .describedAs("예약된 티켓 수가 1이어야 합니다.");
-
-    TicketResponse ticketResponse = ticketResponses.get(0);
-    assertNotNull(ticketResponse.getId(), "티켓 ID는 null이 아니어야 합니다.");
-
-    //TODO: TicketResponse.seatId 추가
-//    assertEquals(seatId, ticketResponse.getSeatId(), "좌석 ID가 일치해야 합니다.");
+  private void cleanUpAll() {
+    jdbcTemplate.execute("delete from ticket");
+    jdbcTemplate.execute("delete from seat");
+    jdbcTemplate.execute("delete from section");
+    jdbcTemplate.execute("delete from schedule");
+    jdbcTemplate.execute("delete from musical");
+    jdbcTemplate.execute("delete from member");
   }
 
   @Test
-  @DisplayName("10명의 사용자가 동시에 티켓을 예매할 경우, 단 1건의 예몌만 성공한다.")
-  public void testBookTicket_multipleUsersSungleSeatSuccress() throws InterruptedException {
-    //given
-    Long seatId = 1L;
+  @DisplayName("모든 테이블에 데이터가 성공적으로 삽입된다.")
+  void testDataInserted() {
+    // Verify member data
+    Long memberCount = jdbcTemplate.queryForObject(
+      "select count(*) from member where email in ('test1@gmail.com', 'test2@gmail.com')",
+      Long.class
+    );
+    assertEquals(2, memberCount, "Both members should be inserted successfully.");
+
+    // Verify musical data
+    Long musicalCount = jdbcTemplate.queryForObject(
+      "select count(*) from musical where title = '킹키부츠' and place = '서울'",
+      Long.class
+    );
+    assertEquals(1, musicalCount, "Musical should be inserted successfully.");
+
+    // Verify schedule data
+    Long scheduleCount = jdbcTemplate.queryForObject(
+      "select count(*) from schedule where date = '2024-10-17' and musical_id = 1",
+      Long.class
+    );
+    assertEquals(1, scheduleCount, "Schedule should be inserted successfully.");
+
+    // Verify section data
+    Long sectionCount = jdbcTemplate.queryForObject(
+      "select count(*) from section where grade = 'R' and price = 190000",
+      Long.class
+    );
+    assertEquals(1, sectionCount, "Section should be inserted successfully.");
+
+    // Verify seat data
+    Long seatCount = jdbcTemplate.queryForObject(
+      "select count(*) from seat where seat_no = 1 and is_available = true",
+      Long.class
+    );
+    assertEquals(1, seatCount, "Seat should be inserted successfully.");
+  }
+
+  @Test
+  @DisplayName("단건 티켓을 예매할 경우, 예매가 성공한다.")
+  void testBookTicket_singleTicketSuccess() {
+    // given
     TicketRequest ticketRequest = new TicketRequest();
-    ticketRequest.setSeatId(List.of(seatId));
-    ticketRequest.setDeviceId(deviceId);
+    ticketRequest.setSeatId(Collections.singletonList(1L));
+    ticketRequest.setDeviceId("deviceID");
 
-    int startNo = 1;
-    members = new ArrayList<>();
-    for (int i = startNo; i < EMAILS.length; i++) {
-      Member member = Member.builder()
-        .email(EMAILS[i])
-        .name("User" + (i + 1))
-        .phone("010123456" + i)
-        .password("password")
-        .pinNumber("1234")
-        .role(RoleType.USER)
-        .isActivated(true)
-        .build();
-      members.add(member);
-    }
-    memberRepository.saveAll(members);
-    if (memberRepository.findByEmail(EMAILS[1]).isPresent()) {
-      System.out.println("email: 1");
-    } else {
-      System.out.println("email: 2");
-    }
+    // when
+    List<TicketResponse> responses = ticketService.bookTicket("test1@gmail.com", ticketRequest);
 
-    testMusical = createTestMusical(musicalRepository);
-    testSchedule = createTestSchedule(scheduleRepository, testMusical);
-    testSection = Section.builder()
-      .grade(Grade.R)
-      .availableSeats(availableSeats)
-      .musical(testMusical)
-      .build();
-    em.persist(testSection);
-    em.flush();
-    Seat seat = Seat.builder()
-      .isAvailable(true)
-      .schedule(testSchedule)
-      .section(testSection)
-      .build();
-    seatRepository.save(seat);
-
-    Long savedSeatId = seat.getId();
-    System.out.println("savedSeatId: " + savedSeatId);
-
-    if (seatRepository.findById(seatId).isPresent()) {
-      System.out.println("seat ok: 2");
-    }
-
-    AtomicInteger successfulBookingsCount = new AtomicInteger(0);
-    int createCnt = EMAILS.length - 1;
-    ExecutorService executorService = Executors.newFixedThreadPool(createCnt);
-    CountDownLatch countDownLatch = new CountDownLatch(createCnt);
-
-    for (int i = startNo; i < EMAILS.length; i++) {
-      String currentEmail = EMAILS[i];
-      executorService.submit(() -> {
-        try {
-          if (memberRepository.findByEmail(currentEmail).isPresent()) {
-            System.out.println("email: 3");
-          } else {
-            System.out.println("email: 4");
-          }
-          System.out.println(currentEmail);
-          //when
-          List<TicketResponse> responses = ticketService.bookTicket(currentEmail, ticketRequest);
-          System.out.println("Booking responses for " + currentEmail + ": " + responses);
-          if (responses.size() > 0) {
-            successfulBookingsCount.incrementAndGet(); // 성공한 예매 카운트 증가
-          }
-        } catch (Exception e) {
-          System.out.println(
-            "Exception during booking for " + currentEmail + ": " + e.getMessage());
-          e.printStackTrace();
-        } finally {
-          countDownLatch.countDown();
-
-        }
-      });
-    }
-    countDownLatch.await();
-    executorService.shutdown();
-
-    //then
-    assertEquals(1, successfulBookingsCount.get(), "동시성 문제: 오직 한 명만 좌석 예약에 성공해야 함");
-    //TODO: 첫번째 사용자 검증 필요
+    // then
+    assertEquals(1, responses.size(), "One ticket response should be returned.");
+    Long ticketCount = jdbcTemplate.queryForObject(
+      "select count(*) from ticket where seat_id = 1 and member_id = 1",
+      Long.class
+    );
+    assertEquals(1, ticketCount, "One ticket should be recorded in the database.");
   }
 
-  private static Musical createTestMusical(MusicalRepository musicalRepository) {
-    return musicalRepository.save(Musical.builder()
-      .title("test Musical")
-      .ranking(1)
-      .place("Main Hall")
-      .placeDetail("1층")
-      .ticketingStartDate(LocalDate.now())
-      .ticketingEndDate(LocalDate.now().plusDays(30))
-      .runningTime(120)
-      .build());
-  }
+  @Test
+  @DisplayName("2명의 사용자가 동시에 티켓을 예매할 경우, 단 1건의 예매만 성공한다.")
+  void testBookTicket_multipleUsersSingleSeatSuccess2() throws InterruptedException {
 
-  private static Schedule createTestSchedule(ScheduleRepository scheduleRepository,
-    Musical musical) {
-    return scheduleRepository.save(Schedule.builder()
-      .musical(musical)
-      .date(LocalDate.of(2024, 10, 22))
-      .startTime(LocalTime.of(20, 0))
-      .build());
+    int threadCount = 2;
+    ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+    CountDownLatch latch = new CountDownLatch(threadCount);
+    List<Exception> exceptions = new ArrayList<>();
+
+    // 첫 번째 사용자
+    String email1 = "test1@gmail.com";
+    TicketRequest request1 = new TicketRequest();
+    request1.setDeviceId("deviceID1");
+    request1.setSeatId(List.of(1L));
+
+    // 두 번째 사용자
+    String email2 = "test2@gmail.com";
+    TicketRequest request2 = new TicketRequest();
+    request1.setDeviceId("deviceID2");
+    request1.setSeatId(List.of(1L));
+
+    // 두 명의 사용자 각각을 별도의 스레드로 실행
+    executor.submit(() -> {
+      try {
+        latch.countDown();
+        latch.await();
+        ticketService.bookTicket(email1, request1);
+      } catch (Exception e) {
+        System.out.println("Exception occurred for " + email1 + ": " + e.getMessage());
+        exceptions.add(e);
+      }
+    });
+
+    executor.submit(() -> {
+      try {
+        latch.countDown();
+        latch.await();
+        ticketService.bookTicket(email2, request2);
+      } catch (Exception e) {
+        System.out.println("Exception occurred for " + email2 + ": " + e.getMessage());
+        exceptions.add(e);
+      }
+    });
+
+    executor.shutdown();
+    executor.awaitTermination(1, TimeUnit.MINUTES);
+
+    // Check the number of tickets created in the ticket table
+    String sql = "select count(*) from ticket";
+    Integer ticketCount = jdbcTemplate.queryForObject(sql, Integer.class);
+    assertEquals(1, ticketCount.intValue());
+
+    // 예외 처리 내용을 확인하거나 로깅
+    if (!exceptions.isEmpty()) {
+      System.out.println("Exceptions occurred during ticket booking:");
+      for (Exception e : exceptions) {
+        System.out.println(e.getMessage());
+      }
+    }
   }
 }
